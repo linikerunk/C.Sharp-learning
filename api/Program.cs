8e789dbde285858225239100f33b5054df681ab2
Liniker de Oliveira <liniker.de.oliveira@avanade.com>
2025-10-29 21:12:03 -0300

    Refactor API routes and update configuration settings routes and enhance configuration management

--- api/Program.cs
+++ api/Program.cs
@@ -1,7 +1,10 @@
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
+var configuration = app.Configuration;
+ProductRepository.Init(configuration);
 
 app.MapGet("/", () => "Hello World! 4");
 app.MapGet("/user", () => new { Name = "Liniker", Age = 26 });
@@ -11,38 +14,67 @@ app.MapGet("/AddHeader", (HttpResponse response) => {
 });
 
 
-app.MapPost("/saveproduct", (Product product) =>
+app.MapPost("/products", (Product product) =>
 {
     ProductRepository.Add(product);
+    return Results.Created($"/products/{product.Code}", product.Code);
 });
 
 
 //api.app.com/users?datastart={date}&dateend={date}
 
-app.MapGet("/getproduct", ([FromQuery] string dateStart, [FromQuery] string dateEnd) =>
+app.MapGet("/products", ([FromQuery] string dateStart, [FromQuery] string dateEnd) =>
 {
     return dateStart + " - " + dateEnd;
 });
 
 //api.app.com/users/{code}
-app.MapGet("/getproduct/{code}", ([FromRoute] string code) =>
+app.MapGet("/products/{code}", ([FromRoute] string code) =>
 {
     var product = ProductRepository.GetBy(code);
-    return product;
+    if (product != null)
+        return Results.Ok(product);
+
+    return Results.NotFound();
 });
 
 
-app.MapGet("/getproductbyheader", (HttpRequest request) =>
+app.MapPut("/products", (Product product) =>
 {
-    return request.Headers["product-code"].ToString();
+    var productSaved = ProductRepository.GetBy(product.Code);
+    productSaved.Name = product.Name;
+    return Results.Ok(productSaved);
 });
+
+app.MapDelete("/products/{code}", ([FromRoute] string code) =>
+{
+    var productSaved = ProductRepository.GetBy(code);
+    ProductRepository.Remove(productSaved);
+    return Results.Ok(productSaved);
+});
+app.MapGet("/configuration/database", (IConfiguration configuration) =>
+{
+   return Results.Ok($"{configuration["database:connection"]}/{configuration["database:port"]}");
+});
+
+// app.MapGet("/getproductbyheader", (HttpRequest request) =>
+// {
+//     return request.Headers["product-code"].ToString();
+// });
+
 
 app.Run();
 
 
 public static class ProductRepository
 {
-    public static List<Product> Products { get; set; }
+    public static List<Product> Products { get; set; } = Products = new List<Product>();
+
+    public static void Init(IConfiguration configuration)
+    {
+        var products = configuration.GetSection("Products").Get<List<Product>>();
+        Products = products;
+    }
 
     public static void Add(Product product)
     {
@@ -59,7 +91,11 @@ public static class ProductRepository
 
         return Products.FirstOrDefault(p => p.Code == code);
     }
-    
+
+    public static void Remove(Product product)
+    {
+        Products.Remove(product);
+    }
 }
 
 public class Product
